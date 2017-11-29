b2be2716b9f7d64574fee812bb3ddcfc613096d9
Lukas Kitsche <l.kitsche@mytaxi.com>
2017-11-29 12:18:20 +0100

    fix url builder

--- S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
@@ -15,6 +15,9 @@
  */
 package com.example.android.sunshine.utilities;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -67,12 +70,19 @@ public final class NetworkUtils {
      */
     public static URL buildUrl(String locationQuery) {
         //DONE TODO (1) Fix this method to return the URL used to query Open Weather Map's API
+        Uri uri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAM, locationQuery)
+                .appendQueryParameter(FORMAT_PARAM, format)
+                .appendQueryParameter(UNITS_PARAM, units)
+                .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                .build();
         URL url = null;
         try {
-            url = new URL(DYNAMIC_WEATHER_URL + "/" + locationQuery);
+            url = new URL(uri.toString());
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+        Log.v("url", url.toString());
         return url;
     }
 
